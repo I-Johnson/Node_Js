a9b891499c6adaaf4c0139a322c129c31b899423
Johnson <subede.johnson@gmail.com>
2022-09-28 17:37:54 -0400

    Yargs command upadate

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,13 +5,26 @@ const chalk = require('chalk')
 
 // Customize Yargs version
 yargs.version('1.1.0')
-
+ 
 // Create add command
 yargs.command({
     command: 'add', 
     describe: 'Add a new note', 
-    handler: function () {
-        console.log('Adding a new note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true, 
+            type: 'string'
+        }, 
+        body: {
+            describe: 'Note Body', 
+            demandOption: true,
+            type: 'string'
+        }, 
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('body: ' + argv.body)
     }
 })
 
@@ -41,5 +54,5 @@ yargs.command({
         console.log('Read the notes')
     }
 })
-
-console.log(yargs.argv)
+yargs.parse()
+// console.log(yargs.argv)
