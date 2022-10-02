61ed3af57bd5af875ee4c50de82b2e06c880df8b
Johnson <subede.johnson@gmail.com>
2022-10-02 10:01:25 -0500

    refactoring arrow

--- notes-app/app.js
+++ notes-app/app.js
@@ -23,7 +23,7 @@ yargs.command({
             type: 'string'
         }, 
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -39,7 +39,7 @@ yargs.command({
             type: 'string'
         },
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -48,7 +48,7 @@ yargs.command({
 yargs.command({
     command: 'list',
     describe: 'listing out the notes',
-    handler: function() {
+    handler(){
         console.log('Listing the notes')
     }
 })
@@ -57,7 +57,7 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe: 'Read the notes',
-    handler: function() {
+    handler(){
         console.log('Read the notes')
     }
 })
